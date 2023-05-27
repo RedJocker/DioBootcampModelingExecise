59957f1343a1268e389b90f0c4466d55e207c1e6
RedJocker <mauricio.guaruja@gmail.com>
2023-05-26 22:30:37 -0300

    add abstract model Content

---
+++ javaBootcamp/src/main/java/model/Content.java
@@ -0,0 +1,23 @@
+package model;
+
+public abstract class Content {
+    public static double DEFAULT_XP = 10.00;
+    
+    private final String title;
+    private final String description;
+
+    public Content(String title, String description) {  
+        this.title = title;                             
+        this.description = description;                 
+    }
+
+    public abstract double getXp();
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+}
