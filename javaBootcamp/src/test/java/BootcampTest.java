99572ed1e7303cfaec16af8cc9af623fc2b3a414
RedJocker <mauricio.guaruja@gmail.com>
2023-05-26 21:18:38 -0300

    build config for both app modules (java and kotlin)

---
+++ javaBootcamp/src/test/java/BootcampTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BootcampTest {
+    @Test
+    void testTest() {
+        assertEquals("test", "test");
+    }
+
+//    @Test
+//    void testFail() {
+//        assertEquals("test", "");
+//    }
+}
---
+++ kotlinBootcamp/src/test/kotlin/BootcampTest.kt
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+
+class BootcampTest {
+    @Test
+    fun testTest() {
+        assertEquals("test", "test")
+    }
+
+//    @Test
+//    fun testFail() {
+//        assertEquals("test", "")
+//    }
+}
