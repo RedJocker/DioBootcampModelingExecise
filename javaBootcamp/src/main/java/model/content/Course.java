2c8d6cfaf82862f580a186bc6b3b33d1a2b86cf7
RedJocker <mauricio.guaruja@gmail.com>
2023-05-27 01:00:28 -0300

    pretty toString

--- javaBootcamp/src/main/java/model/content/Course.java
+++ javaBootcamp/src/main/java/model/content/Course.java
@@ -1,5 +1,8 @@
 package model.content;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Course extends Content  {
 
     private final int totalHours;
@@ -16,11 +19,33 @@ public class Course extends Content  {
 
     @Override
     public String toString() {
-        return "Course{" +
-                "title='" + getTitle() + '\'' +
-                ", description='" + getDescription() + '\'' +
-                ", totalHours=" + totalHours +
-                ", xpAwarded=" + xpAwarded() +
-                '}';
+        final int descLength = getDescription().length();
+        char a = 'a';
+        final int lineBreak = 75;
+        final String fmt =
+                "================================================================================\n" +
+                        "| Course: %-69s|\n" +
+                        "| Total Hours: %-64s|\n" +
+                        "| Awarded XP: %-65.2f|\n" +
+                        "================================================================================\n" +
+                        Stream.iterate(0, i -> i < descLength, i -> i + lineBreak)
+                                .map(i -> getDescription().substring(i, Math.min(i + lineBreak + 1, descLength)))
+                                .map(str -> wrappedDescriptionLine(str, lineBreak))
+                                .collect(Collectors.joining()) +
+                        "================================================================================\n"
+                ;
+
+        return String.format(fmt, getTitle(), totalHours, xpAwarded());
+    }
+
+    private String wrappedDescriptionLine(String descriptionLineWithFirstCharOfNext, int lineBreak) {
+        final String d = descriptionLineWithFirstCharOfNext;
+
+        final int lastIndex = Math.min(d.length(), lineBreak);
+        final boolean shouldPutWrapSign = lastIndex == lineBreak
+                && Character.isLetter(d.charAt(lineBreak - 1))
+                && Character.isLetter(d.charAt(lineBreak));
+
+        return String.format("| %-75s%s |\n", d.substring(0, lastIndex).trim(),  shouldPutWrapSign ? "-" : " ");
     }
 }
